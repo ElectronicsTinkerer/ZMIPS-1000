a928d8471b99d2031cddf13d04d150f9ec0c3e75
Zach Baldwin <electronicstinkerer@gmail.com>
2022-04-22 15:40:51 -0400

    Successfully drawing a sprite with mask

--- asm/game_source.asm
+++ asm/game_source.asm
@@ -75,7 +75,7 @@
     add r29, r29, r0
     
     ; Save X
-    mov r12, r20
+    mov r17, r20
 
     ; calculate line
     ; y * line_width
@@ -84,27 +84,83 @@
     jpl mult
     
     ; Determine line index
-    srl r21, r12, 3     ; Divide out pixel number (FIXME)
+    srl r21, r17, 3     ; Divide out pixel number (FIXME)
     ffl X               ; Clear C
     add r20, r20, r21
 
-    ; Currently, r20 stores the base offset of the video data word
-    lw r13, r22         ; Get the sprite's line of data
-    lw r14, r22         ; Get the sprite's corresponding line mask - FIX
-    and r13, r13, r14
+    ; BEGIN LOOP
+    li 0                ; Init loop var
+    mov r10, r0
+    
+    li 1
+    ffl C
+    sub r22, r22, r0    ; Start sprite pointer at address minus 1
 
+:sprite_loop
+    ; Currently,
+    ;    r10 stores the loop count
+    ;    r20 stores the base offset of the video data word
+    ;    r22 stores the pointer to the sprite data base
+    ffl x
+    li 1
+    add r22, r22, r0    ; Increment sprite data pointer
+    lw r13, r22         ; Get the sprite's line of data
+    ffl x
+    li SPRITE_MASK_OFFSET
+    add r0, r22, r0
+    lw r14, r0          ; Get the sprite's corresponding line mask
+    and r13, r13, r14   ; Apply the mask
+
+    ; Regs used below:
+    ;    r13 - Sprite low data
+    ;    r15 - Sprite high data
+    ;    r14 - Sprite low mask
+    ;    r16 - Sprite high mask
+    ;    r17 - Pixel horiz word offset
+    ;    r21 - Jump address for LUTs
+    mov r15, r13
+    mov r16, r14
+    ; Handle high data/mask shifting
     li 7
-    and r12, r12, r0    ; Mask off index into word
-    eor r21, r12, r0    ; Invert index bits (reverse count direction)
+    and r17, r17, r0    ; Mask off index into word
+    mov r21, r17        ; Copy into jump reg
     sll r21, r21, 3     ; jump index * 2 * 4
-    li sprite_shift_lut
+    li sprite_shift_lut_high
     ffl x
     add r21, r21, r0
     jmp r21
 
-:sprite_shift_lut
-    sll r13, r13, 4     ; 28 Shift both the data
-    sll r14, r14, 4     ;  and the mask
+:sprite_shift_lut_high
+    srl r15, r15, 4     ; 28 Shift data
+    srl r16, r16, 4     ; Same for the mask
+    srl r15, r15, 4     ; 24
+    srl r16, r16, 4
+    srl r15, r15, 4     ; 20
+    srl r16, r16, 4
+    srl r15, r15, 4     ; 16
+    srl r16, r16, 4 
+    srl r15, r15, 4     ; 12
+    srl r16, r16, 4
+    srl r15, r15, 4     ; 8
+    srl r16, r16, 4
+    srl r15, r15, 4     ; 4
+    srl r16, r16, 4
+    srl r15, r15, 4     ; 0
+    srl r16, r16, 4
+    
+    ; Handle low data/mask shifting
+:sprite_shift_low
+    li 7
+    eor r21, r17, r0    ; Invert index bits (reverse count direction)
+    sll r21, r21, 3     ; jump index * 2 * 4
+    li sprite_shift_lut_low
+    ffl x
+    add r21, r21, r0
+    jmp r21
+
+:sprite_shift_lut_low
+    sll r13, r13, 4     ; 28 Shift data
+    sll r14, r14, 4     ; Same for the mask
     sll r13, r13, 4     ; 24
     sll r14, r14, 4
     sll r13, r13, 4     ; 20
@@ -117,25 +173,46 @@
     sll r14, r14, 4
     sll r13, r13, 4     ; 4
     sll r14, r14, 4
-    nop                 ; 0
-    nop
-    
+    ;nop                ; 0
+    ;nop
 
+    ; Read vmem and get the data present under the current sprite location
+    ; Regs:
+    ;    r10 - loop index
+    ;    r11 - vmem data low
+    ;    r12 - vmem data high
+    ;    r13 - Sprite low data
+    ;    r15 - Sprite high data
+    ;    r14 - Sprite low mask
+    ;    r16 - Sprite high mask
+    ;    r17 - Pixel horiz word offset
+    ;    r20 - vmem addr low
+    ;    r21 - vmem addr high
+    ;    r22 - Sprite data pointer
+    li 0                ; Calculate high byte address
+    ffl c
+    add r21, r20, r0
+    lw r11, r20         ; Get low word of vdata
+    lw r12, r21         ; Get high word of vdata
+    li 0xffffffff       ; Invert the masks
+    eor r14, r14, r0
+    eor r16, r16, r0
+    and r11, r11, r14   ; And apply them to vmem
+    and r12, r12, r16
+    or r11, r11, r13    ; Or the sprite data into vmem
+    or r12, r12, r15
+    sw r20, r11
+    sw r21, r12
 
-    ; li SPRITE_PLAYER_DATA     ; Sprite to display
-    lw r12, r22
-    li SPRITE_PLAYER_MASK       ; Calculate mask address
+    ; Next byte of data
     ffl x
-    add r22, r22, r0
-    lw r0, r22
-    ; and r12, r12, r0            ; Mask off sprite data
-;     li 0x60
-;     mov r20, r0
-    ; li 0xffffffff
-;     li 0
-    and r13, r14, r13
-    sw r20, r13
-
+    li LINE_WIDTH       ; Move vmem pointer to next line
+    add r20, r20, r0
+    li 1                ; Increment loop var
+    add r10, r10, r0
+    li 8                ; 8 iterations (8 pixels tall)
+    cmp r10, r0, Z
+    bfc Z,sprite_loop
     
     li 1                ; Return
     ffl x
